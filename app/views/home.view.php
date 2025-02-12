f1c764c7ee1c41cf4ee3b2b58d5b0bc6d8c77a5c
dadssi <dadssi.mohamed.abdelhak@gmail.com>
2025-02-12 11:02:21 +0000

    test

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -46,7 +46,7 @@
             <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
                 <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
                     <div class="icon p-2 me-2">
-                        <img class="img-fluid" src="<?php echo ROOT ?>/assets/img/icon-deal.png" alt="Icon" style="width: 30px; height: 30px;">
+                        <img class="img-fluid" src="<?php echo ROOT ?>/assets/img/logo-easymatch.png" alt="Icon" style="width: 30px; height: 30px;">
                     </div>
                     <h1 class="m-0 text-primary">EasyMatch</h1>
                 </a>
